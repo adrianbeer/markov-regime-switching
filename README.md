884852415b2b3dac3ecc85276071ce6bbd924423
somebody <adrianbeer@fastmail.com>
2022-07-29 17:07:36 +0200

    Adds more potential datasets to README and more statistical analysis of the datasets

--- README.md
+++ README.md
@@ -6,14 +6,13 @@ Donwload the data specified in [Data Source](#Data-Source) to some folder.
 Go to the [configuration file](config.py) and set `data_dir` to that folder.
 
 
-# Data Source
+# Data Source (with Great Depression)
 - S&P data set by Robert Shiller: [Website](http://www.econ.yale.edu/~shiller/data.htm),
-[Download](http://www.econ.yale.edu/~shiller/data/ie_data.xls).
-
+[Download](http://www.econ.yale.edu/~shiller/data/ie_data.xls). **The stock price data are monthly averages of daily closing prices.**
 - Industrial Production Total Index: [Website](https://fred.stlouisfed.org/series/INDPRO), downloaded as `.csv`.
-
-
-
+- Moody's Seasoned Baa Corporate Bond Yield (BAA) [Website](https://fred.stlouisfed.org/series/BAA)
+- Moody's Seasoned Aaa Corporate Bond Yield (AAA) [Website](https://fred.stlouisfed.org/series/AAA)
+- Producer Price Index by Commodity: All Commodities (PPIACO)
 
 # Useful commands:
 activate markov-switching
--- dataset_preprocessor.py
+++ dataset_preprocessor.py
@@ -1,9 +1,11 @@
+import os
+
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-import os
-from config import data_dir
+from sklearn.decomposition import PCA
 
+from config import data_dir
 
 # Schiller S&P TR
 # ----------------
@@ -21,27 +23,77 @@ data['Datetime'] = pd.to_datetime(data.iloc[:, 0], format="%Y.%m.%d")
 data = data.set_index('Datetime').sort_index()
 
 
-data = (data.iloc[:, 1])/ data.iloc[:, 4]
+data = (data.iloc[:, 1])# / data.iloc[:, 4]
 data = pd.to_numeric(data)
+SP_TR_df = data
 
-print(data.tail(20))
+print(np.log(SP_TR_df).diff().dropna().describe())
+plt.hist(np.log(SP_TR_df).diff().dropna(), bins="scott")
+plt.show()
 
-f, ax = plt.subplots(1,1)
-ax.plot(np.log(data))
-f.show()
+plt.plot(np.log(SP_TR_df))
+plt.show()
 
-# EMRATIO Data set
+# FRED Data sets
 # ----------------
-path = os.path.join(data_dir, "EMRATIO.csv")
-df = pd.read_csv(path)
-df["DATE2"] = pd.to_datetime(df.pop("DATE"))
-df = df.set_index("DATE2")
+dfs = []
+for f_name in ["BAA", "AAA"]:
+    path = os.path.join(data_dir, f"{f_name}.csv")
+    df = pd.read_csv(path)
+    df["DATE2"] = pd.to_datetime(df.pop("DATE"))
+    df = df.set_index("DATE2")
+    dfs.append(df)
 
-ax2 = ax.twinx()
-ax2.plot(df, color='red')
-f.show()
-
+# Feature engineering
+# --------------------
+FRED_df = pd.concat(dfs, axis=1, join="inner")
+FRED_df["SPREAD"] = (FRED_df["BAA"] - FRED_df["AAA"])
+FRED_df = FRED_df.drop(columns=["AAA"])
+indicator_df = np.log(FRED_df).diff().dropna()
 
 # Show data
 # --------------
+# f, ax = plt.subplots(1,1)
+# ax.plot(np.log(SP_TR_df))
+#
+# ax2 = ax.twinx()
+# ax2.plot(np.log(FRED_df), color='green', linestyle='dashed')
+# plt.show()
+
+
+# Principle Component analysis
+# ----------------------------
+plt.plot(indicator_df, linewidth=0.5)
 plt.show()
+
+pca = PCA(1)
+pca.fit(indicator_df)
+component = pd.Series(index=indicator_df.index, data=pca.transform(indicator_df).flatten())
+length = len(component)
+
+#plt.close()
+plt.plot(component, label="1st PC", linewidth=0.5)
+plt.plot(np.log(SP_TR_df).diff()[-length:], linewidth=0.5)
+plt.axhline(0, color='red')
+plt.legend()
+plt.show()
+
+print(np.corrcoef(np.log(SP_TR_df).diff()[-length:], component))
+
+# Testing the Autoregressive assumption on the dynamic factor component
+# ----------------------------------------------------------------------
+import statsmodels.api as sm
+from statsmodels.stats.diagnostic import het_white
+
+exog = np.vstack((np.repeat(1, len(component)-2), component[:-2], component[1:-1])).T
+
+ar = sm.OLS(component[2:], exog).fit()
+print(ar.params)
+print(ar.pvalues)
+plt.plot(ar.resid)
+plt.show()
+
+_, lm_p, _, f_p = het_white(ar.resid, exog)
+print(lm_p)
+print(f_p)
+
