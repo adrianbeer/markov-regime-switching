21cc4fabe7537dc99ea3dd2d8fef1d384154d1c2
adrianbeer <adrianbeer@fastmail.com>
2022-09-24 17:29:13 +0200

    ...

--- markov_model.R
+++ markov_model.R
@@ -6,12 +6,21 @@ library(latex2exp)
 library("MSGARCH")
 library(patchwork)
 require(gridExtra)
+library(murphydiagram)
+library(forecast)
+library(xtable)
+library(fGarch)
+
 
 theme_update(plot.title = element_text(hjust = 0.5))
 msgarch_color = "green"
 garch_color = "red"
+mssr1_color = "brown"
+mssr2_color = "blue"
 
-# TODO: Include random walk error
+# TODO: Include scatter plot of returns/ Realized Vola
+# TODO: Analyze Subsample performance
+# TODO: Compare special state performance
 
 data_dir = "C:\\Users\\Adria\\Documents\\Github Projects\\data\\markov-switching"
 img_dir = "C:\\Users\\Adria\\Documents\\Github Projects\\markov-regime-switching\\LaTeX\\img"
@@ -22,13 +31,11 @@ df <- quantmod::getSymbols("^GSPC", src = "yahoo", from = start, to = end, auto.
 price = df[, "GSPC.Close"]
 colnames(price) <- "Price"
 lret = diff(log(price))[-1,]
+lret = lret - mean(lret) # Demeaning -> MSGARCH assumption
 plot(lret)
 
 
-corona_indices <- seq.Date(as.Date("2020-01-01"),as.Date("2022-05-01"),by="day")
-housing_bubble <- seq.Date(as.Date("2008-01-01"),as.Date("2010-01-01"),by="day")
-russia_war <- seq.Date(as.Date("2021-01-01"),as.Date("2023-01-01"),by="day")
-oil <- seq.Date(as.Date("1986-01-01"),as.Date("1991-01-01"),by="day")
+
 
 lret = lret*100 # For numerical purposes
 
@@ -46,6 +53,16 @@ bear_test_dates <- c(seq.Date(as.Date("2020-02-01"),as.Date("2020-04-01"),by="da
                      seq.Date(as.Date("2021-01-01"),as.Date("2023-01-01"),by="day"))
 bull_test_dates <- index(test_vol)[!(index(test_vol) %in% bear_test_dates)]
   
+
+corona_indices <- seq.Date(as.Date("2020-01-01"),as.Date("2022-05-01"),by="day")
+housing_bubble <- seq.Date(as.Date("2008-01-01"),as.Date("2010-01-01"),by="day")
+russia_war <- seq.Date(as.Date("2021-01-01"),as.Date("2023-01-01"),by="day")
+oil <- seq.Date(as.Date("1986-01-01"),as.Date("1991-01-01"),by="day")
+
+
+eighties <- seq.Date(as.Date("1986-01-01"),as.Date("1990-01-01"),by="day")
+
+
 # # Imports OMI realized volatility
 # omi <- read.csv(paste(data_dir, "\\oxfordmanrealizedvolatilityindices.csv", sep=""))
 # omi <- omi[omi$Symbol==".SPX", ]
@@ -68,7 +85,7 @@ start.time <- Sys.time()
 
 # SINGLE-REGIME 
 garch.n <- CreateSpec(variance.spec = list(model = c("gjrGARCH")),
-                          distribution.spec = list(distribution = c("sstd")))
+                          distribution.spec = list(distribution = c("sged")))
 summary(garch.n)
 garch.fit.ml <- FitML(spec = garch.n, data = train_y)
 summary(garch.fit.ml)
@@ -79,6 +96,30 @@ ms2.garch.n <- CreateSpec(variance.spec = list(model = c("gjrGARCH", "gjrGARCH")
 summary(ms2.garch.n)
 msgarch.fit.ml <- FitML(spec = ms2.garch.n, data = train_y)
 summary(msgarch.fit.ml)
+sr.fit <- ExtractStateFit(msgarch.fit.ml)
+
+# Make LaTeX table of parameters
+params <- cbind(sr.fit[[1]]$par, sr.fit[[2]]$par, garch.fit.ml$par)
+params <- as.data.frame(params)
+colnames(params) <- c("MSSR1", "MSSR2", "GARCH")
+xtable(params)
+
+# Visualize densities
+f <- function(x) { return(dsged(x, 0, 1, nu=params[5, 1], xi=params[4, 1])) }
+g <- function(x) { return(dsged(x, 0, 1, nu=params[5, 2], xi=params[4, 2])) } # TODO: use params here
+h <- function(x) { return(dsged(x, 0, 1, nu=params[5, 3], xi=params[4, 3])) }
+x_grid <- seq(-5, 5, by=10/500)
+y_values <- sapply(x_grid, f)
+ggplot() + 
+  geom_line(aes(x=x_grid, y=sapply(x_grid, f), color="MSSR1")) + 
+  geom_line(aes(x=x_grid, y=sapply(x_grid, g), color="MSSR2" )) + 
+  geom_line(aes(x=x_grid, y=sapply(x_grid, h), color="GARCH")) + 
+  scale_color_manual(name = "Model", 
+                     values = c("GARCH"=garch_color, "MSSR1"=mssr1_color, "MSSR2"=mssr2_color)) + 
+  ggtitle("Fitted standardized distributions for the log-returns") +
+  ylab("f(y)") +
+  xlab("y")
+ggsave(paste(img_dir, "\\FittedStandardizedDistr.pdf", sep=""))
 
 
 # Unconditional volatility levels
@@ -107,58 +148,132 @@ p1 <- ggplot() +
 p1
 #ggsave(paste(img_dir, "\\PredProb_State1_InSample.pdf", sep=""))
 
-# In-sample volatility
+
+# -------------------- In-sample volatility estimate comparison (state1)
+
 msgarch_train_vola <- Volatility(msgarch.fit.ml) %>% fortify.zoo
 garch_train_vola <- Volatility(garch.fit.ml) %>% fortify.zoo
 
 
-p2 <- ggplot(msgarch_train_vola) + geom_line(aes(x=Index, y=.)) + 
+p2 <- ggplot() + 
+  geom_line(dat=msgarch_train_vola[msgarch_train_vola$Index %in% eighties, ], aes(x=Index, y=., colour="MSGARCH")) + 
+  geom_line(dat=garch_train_vola[garch_train_vola$Index %in% eighties, ], aes(x=Index, y=., colour="GARCH")) + 
   ylab("Vola") + 
-  ggtitle("Filtered in-sample Volatility") + 
-  scale_x_date(date_breaks = "2 years" , date_labels = "%y")
-
+  ggtitle("Filtered in-sample Volatility during late eighties") + 
+  scale_x_date(date_breaks = "2 years" , date_labels = "%y") + 
+  scale_color_manual(name = "Model", values = c("GARCH"=garch_color, "MSGARCH"=msgarch_color))
 p2
-#ggsave(paste(img_dir, "\\FiltVola_InSample.pdf", sep=""))
+ggsave(paste(img_dir, "\\FiltVola_InSample_Eighties.pdf", sep=""))
 
-grid.arrange(p1, p2, nrow=2)
+#grid.arrange(p1, p2, nrow=2)
 
 
 # ---------------------- Creating Point Forecasts ------------------------------
 # ------------------------------------------------------------------------------
 
 # Calculating the Point forecasts took approx. 1 min.
-start.time <- Sys.time()
+
 horizons = 1:10 # currently only works for horizon=1!!
 
+start.time <- Sys.time()
 
+testing_anchor_points <- seq(1, (length(test_y)-max(horizons)), max(horizons))
+
+
+# -------------------- MSGARCH and GARCH
+# models <- list(msgarch.fit.ml, garch.fit.ml)
+# fcst <- xts(x = cbind(replicate(length(horizons)*length(models), rep(-9999, length(test_y)))), 
+#                order.by = index(test_y))
+# 
+# names(fcst) = c(sapply(horizons, function(x) paste("MSGARCH_h", toString(x), sep="")),
+#                 sapply(horizons, function(x) paste("GARCH_h", toString(x), sep="")))
+# 
+
+# for (mm in 1:length(models)) {
+#   fcst[1, 1] <- predict(models[[mm]], nahead = 1, do.return.draw = FALSE)$vol[1]
+#   for (dd in 1:(length(test_y)-1)) {
+#     pred <- predict(models[[mm]], nahead = 1, do.return.draw = FALSE, newdata=test_y[1:dd])
+#     fcst[dd+1, (mm-1)*(length(horizons))+1] = pred$vol[1] # Note that the dates in pred$vol aren't correct - they include weekend
+#   } 
+#   for (dd in testing_anchor_points) {
+#     pred <- predict(models[[mm]], nahead = max(horizons), do.return.draw = FALSE, newdata=test_y[1:dd])
+#     for (hh in 2:length(horizons)) {
+#       fcst[dd+hh, (mm-1)*(length(horizons))+hh] = pred$vol[horizons[hh]]
+#     }
+#   }  
+# }
+
+# ---------------------- Single regime
 fcst <- xts(x = cbind(replicate(length(horizons), rep(-1, length(test_y)))), 
-               order.by = index(test_y))
+            order.by = index(test_y))
 names(fcst) = sapply(horizons, function(x) paste("GARCH_h", toString(x), sep=""))
 
-#SINGLE-REGIME
-for (dd in 1:(length(test_y)-max(horizons))) {
+fcst[1, 1] <- predict(garch.fit.ml, nahead = 1, do.return.draw = FALSE)$vol[1]
+for (dd in 1:(length(test_y)-1)) {
+  pred <- predict(garch.fit.ml, nahead = 1, do.return.draw = FALSE, newdata=test_y[1:dd])
+  fcst[dd+1, 1] = pred$vol[1] # Note that the dates in pred$vol aren't correct - they include weekend
+} 
+for (dd in testing_anchor_points) {
   pred <- predict(garch.fit.ml, nahead = max(horizons), do.return.draw = FALSE, newdata=test_y[1:dd])
-  for (hh in 1:length(horizons)) {
-    stopifnot(index(fcst)[dd+h] == index(pred$vol)[hh])
-    fcst[dd+hh,] = pred$vol[hh]
+  for (hh in 2:length(horizons)) {
+    fcst[dd+hh, hh] = pred$vol[horizons[hh]]
   }
 }  
 garch_fcst <- fcst
+
+# --------------------- MSGARCH SINGLE-REGIMES
+# SR FIT 1
+model <- sr.fit[[1]]
+fcst <- xts(x = cbind(rep(-1, length(test_y))), 
+            order.by = index(test_y))
+names(fcst) = "MSSR1_h1"
+
+fcst[1, 1] <- predict(model, nahead = 1, do.return.draw = FALSE)$vol[1]
+for (dd in 1:(length(test_y)-1)) {
+  pred <- predict(model, nahead = 1, do.return.draw = FALSE, newdata=test_y[1:dd])
+  fcst[dd+1, 1] = pred$vol[1] # Note that the dates in pred$vol aren't correct - they include weekend
+} 
+sr1_fcst <- fcst
+
+# SR FIT 2
+model <- sr.fit[[2]]
+fcst <- xts(x = cbind(rep(-1, length(test_y))), 
+            order.by = index(test_y))
+names(fcst) = "MSSR2_h1"
+
+fcst[1, 1] <- predict(model, nahead = 1, do.return.draw = FALSE)$vol[1]
+for (dd in 1:(length(test_y)-1)) {
+  pred <- predict(model, nahead = 1, do.return.draw = FALSE, newdata=test_y[1:dd])
+  fcst[dd+1, 1] = pred$vol[1] # Note that the dates in pred$vol aren't correct - they include weekend
+} 
+sr2_fcst <- fcst
+
+# ----------------------- MULTI-REGIME
 
 fcst <- xts(x = cbind(replicate(length(horizons), rep(-1, length(test_y)))), 
             order.by = index(test_y))
 names(fcst) = sapply(horizons, function(x) paste("MSGARCH_h", toString(x), sep=""))
 
-#MULTI-REGIME
-for (dd in 1:(length(test_y)-max(horizons))) {
+fcst[1, 1] <- predict(msgarch.fit.ml, nahead = 1, do.return.draw = FALSE)$vol[1]
+for (dd in 1:(length(test_y)-1)) {
+  pred <- predict(msgarch.fit.ml, nahead = 1, do.return.draw = FALSE, newdata=test_y[1:dd])
+  fcst[dd+1, 1] = pred$vol[1] # Note that the dates in pred$vol aren't correct - they include weekend
+} 
+for (dd in testing_anchor_points) {
   pred <- predict(msgarch.fit.ml, nahead = max(horizons), do.return.draw = FALSE, newdata=test_y[1:dd])
-  fcst[dd+1,] = pred$vol[horizons]
+  for (hh in 2:length(horizons)) {
+    fcst[dd+hh, hh] = pred$vol[horizons[hh]]
+  }
 }  
 msgarch_fcst <- fcst
+
 
 end.time <- Sys.time()
 time.taken <- end.time - start.time
 time.taken
+
+saveRDS()
+
 
 
 
@@ -179,29 +294,78 @@ f_rmse <- function(y_hat, y) {
   return (sqrt(f_mse(y_hat, y)))
 }
 get_err_table <- function(y_hat, y) {
-  mse <- sapply(y_hat, f_mse, y)
-  mae <- sapply(y_hat, f_mae, y)
-  bias <- sapply(y_hat, f_bias, y)
-  rmse <- sapply(y_hat, f_rmse, y)
-  
+  if (is.null(dim(y_hat))) {
+    mse <- f_mse(y_hat, y)
+    mae <- f_mae(y_hat, y)
+    bias <- f_bias(y_hat, y)
+    rmse <- f_rmse(y_hat, y) 
+  } else {
+    mse <- sapply(y_hat, f_mse, y)
+    mae <- sapply(y_hat, f_mae, y)
+    bias <- sapply(y_hat, f_bias, y)
+    rmse <- sapply(y_hat, f_rmse, y)  
+  }
   err_table <- cbind(mse, mae, rmse, bias)
   return(err_table)
 }
 
+
 # ---------------------------- Out of sample -------------------------------
-msgarch_err <- get_err_table(msgarch_fcst, test_vol)
-msgarch_err
-
-garch_err <- get_err_table(garch_fcst, test_vol)
-garch_err
-
+msgarch_err <- get_err_table(msgarch_fcst[,1], test_vol)
+garch_err <- get_err_table(garch_fcst[,1], test_vol)
 avg_err <- get_err_table(mean(train_vol), test_vol)
-avg_err
 rownames(avg_err) = "Avg"
 
-rbind(msgarch_err, garch_err, avg_err)
-# ---------------------------- In sample -------------------------------
+sr1_err <- get_err_table(sr1_fcst, test_vol)
+sr2_err <- get_err_table(sr1_fcst, test_vol)
 
+rbind(msgarch_err, garch_err, sr1_err, sr2_err, avg_err)
+
+# - Diebold-Mariano Test
+dm.test(msgarch_fcst[,1], garch_fcst[,1], h=1, power = 1)
+dm.test(msgarch_fcst[,1], garch_fcst[,1], h=1, power = 2)
+
+# - Murphy Diagram for one-day-ahead point forecasts
+murphydiagram(as.vector(msgarch_fcst[,1]), as.vector(garch_fcst[,1]), as.vector(test_vol$Price), labels=c("MSGARCH", "GARCH"))
+
+# ---------------------------- In sample -------------------------------
+bip <- 100 # burn_in_period
+msgarch_train_vola <- Volatility(msgarch.fit.ml)
+garch_train_vola <- Volatility(garch.fit.ml)
+mssr1_train_vola <- Volatility(sr.fit[[1]])
+mssr2_train_vola <- Volatility(sr.fit[[2]])
+partial_avg <- (cumsum(train_vol)/seq_along(train_vol))$Price %>% as.zooreg
+
+forecasts <- list(msgarch_train_vola, garch_train_vola, mssr1_train_vola, mssr2_train_vola, partial_avg)
+err_tables <- list()
+for (ff in 1:length(forecasts)) {
+  err_tables[[ff]] <- get_err_table(forecasts[[ff]][bip:length(train_vol),], train_vol[bip:length(train_vol),] %>% as.zooreg)
+}
+do.call("rbind", err_tables)
+
+# average has bias, because we start with the 1929 depression...
+
+# ---------------------------- Eighties Case Study
+actual <- train_vol[index(train_vol) %in% eighties]
+
+ms_garch_eighties <- msgarch_train_vola[msgarch_train_vola$Index %in% eighties, 2]
+garch_eighties <- garch_train_vola[garch_train_vola$Index %in% eighties, 2]
+mssr1_eighties <- mssr1_train_vola[mssr1_train_vola$Index %in% eighties, 2]
+mssr2_eighties <- mssr2_train_vola[mssr2_train_vola$Index %in% eighties, 2]
+start_of_eighties_idx <- match(as.Date(eighties[2]), as.Date(index(train_vol)))
+avg_eighties <- mean(train_vol[1:start_of_eighties_idx])
+
+forecasts <- list(ms_garch_eighties, garch_eighties, mssr1_eighties, mssr2_eighties, avg_eighties)
+err_tables <- list()
+for (ff in 1:length(forecasts)) {
+  err_tables[[ff]] <- get_err_table(forecasts[[ff]], actual)
+}
+
+eighties_err_tabl <- do.call("rbind", err_tables)
+rownames(eighties_err_tabl) <- c("MSGARCH", "GARCH", "MSSR1", "MSSR2", "Avg")
+eighties_err_tabl
+
+rbind(msgarch_err, garch_err, sr1_err, sr2_err, avg_err)
 
 # --------------- Scenario Forecast evaluation etc. ----------------------------
 # --------------- Latent State evaluation --------------------------------------
@@ -215,7 +379,25 @@ rbind(msgarch_err, garch_err, avg_err)
 # dummy_msgarch.fit.ml <- FitML(spec = dummy_ms2.garch.n, data = train_y)
 # summary(dummy_msgarch.fit.ml)
 
-# -------------------------  Corona Case Study --------------------------------
+# -------------------------  In-Sample  --------------------------------
+
+# In-sample volatility
+
+msgarch_train_vola <- Volatility(msgarch.fit.ml) %>% fortify.zoo
+garch_train_vola <- Volatility(garch.fit.ml) %>% fortify.zoo
+
+
+p2 <- ggplot(msgarch_train_vola) + geom_line(aes(x=Index, y=.)) + 
+  ylab("Vola") + 
+  ggtitle("Filtered in-sample Volatility") + 
+  scale_x_date(date_breaks = "2 years" , date_labels = "%y")
+
+p2
+#ggsave(paste(img_dir, "\\FiltVola_InSample.pdf", sep=""))
+
+
+
+# -------------------------  Out-Of-Sample  --------------------------------
 
 # CORONA POINT FORECAST
 p1 <- ggplot() + 
@@ -226,9 +408,22 @@ p1 <- ggplot() +
   xlab("Dates (Year)") + 
   ylab("Prob") + 
   ggtitle("1-day-ahead volatility forecasts during the Corona Crash")
-
 p1
 #ggsave(paste(img_dir, "\\PredProb_State1_InSample.pdf", sep=""))
+
+
+# CORONA POINT FORECAST - MSGARCH Decomposition
+p1 <- ggplot() + 
+  geom_line(data=sr1_fcst[index(msgarch_fcst) %in% corona_indices, ], aes(x=Index, y=MSSR1_h1, colour="mssr1")) + 
+  geom_line(data=sr2_fcst[index(msgarch_fcst) %in% corona_indices, ], aes(x=Index, y=MSSR2_h1, colour="mssr2")) + 
+  geom_line(data=garch_fcst[index(msgarch_fcst) %in% corona_indices, ], aes(x=Index, y=GARCH_h1, colour="garch")) +
+  scale_color_manual(name = "Model", values = c("garch"=garch_color, "mssr1"="brown", "mssr2"="green")) + 
+  scale_x_date(date_breaks = "2 years" , date_labels = "%y") + 
+  xlab("Dates (Year)") + 
+  ylab("Prob") + 
+  ggtitle("1-day-ahead volatility forecasts during the Corona Crash")
+p1
+ggsave(paste(img_dir, "\\CoronaCrash_MSGARCH_Decomposition.pdf", sep=""))
 
 # RUSSIA WAR POINT FORECAST
 p1 <- ggplot() + 
